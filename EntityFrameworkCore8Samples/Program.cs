b10366e3daf1e497ede4bd3390b3d34f78aa2b86
Marco Minerva <marco.minerva@gmail.com>
2023-11-02 10:35:28 +0100

    Add sample log configuration

--- EntityFrameworkCore8Samples/Program.cs
+++ EntityFrameworkCore8Samples/Program.cs
@@ -1,3 +1,4 @@
 ﻿/*
 Scaffold-DbContext 'Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MinimalDB' Microsoft.EntityFrameworkCore.SqlServer -ContextDir DataAccessLayer -OutputDir 'DataAccessLayer/Entities'
+optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
 */
