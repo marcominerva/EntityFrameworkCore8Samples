470a1905e285fa22bd5cb16f30fa3a7422488e61
Marco Minerva <marco.minerva@gmail.com>
2023-11-02 10:57:38 +0100

    Add first example

---
+++ EntityFrameworkCore8Samples/Collections/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkCore8Samples.Collections.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public IList<string> Tags { get; set; }
+}
---
+++ EntityFrameworkCore8Samples/Collections/SampleDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkCore8Samples.Collections.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EntityFrameworkCore8Samples.Collections;
+
+public partial class SampleDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public SampleDbContext()
+    {
+    }
+
+    public SampleDbContext(DbContextOptions<SampleDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SampleDB");
+        optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+    }
+}
