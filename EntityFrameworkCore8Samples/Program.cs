af13fdd877e01f1b1bdfef6126d40d2c695563ea
Marco Minerva <marco.minerva@gmail.com>
2023-11-15 12:20:53 +0100

    Base sample update

--- EntityFrameworkCore8Samples/Program.cs
+++ EntityFrameworkCore8Samples/Program.cs
@@ -1,4 +1,8 @@
 ﻿/*
 Scaffold-DbContext 'Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MinimalDB' Microsoft.EntityFrameworkCore.SqlServer -ContextDir DataAccessLayer -OutputDir 'DataAccessLayer/Entities'
 optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+add-migration Initial -Context EntityFrameworkCore8Samples.Collections.SampleDbContext
+update-database -Context EntityFrameworkCore8Samples.Collections.SampleDbContext
 */
+
+Console.ReadLine();
